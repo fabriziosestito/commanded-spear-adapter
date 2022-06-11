871efcbd96848ccae9efae7caff8cffac8242ca3
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-06-11 22:11:05 +0200

    Remove deprecation warnings

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Mix.Config
 
 config :ex_unit,
   capture_log: true,
@@ -9,19 +9,3 @@ config :ex_unit,
 config :commanded,
   assert_receive_event_timeout: 5_000,
   refute_receive_event_timeout: 1_000
-
-# config :commanded_extreme_adapter, ExtremeApplication,
-#   event_store: [
-#     adapter: Commanded.EventStore.Adapters.Extreme,
-#     serializer: Commanded.Serialization.JsonSerializer,
-#     stream_prefix: "commandedtest",
-#     extreme: [
-#       db_type: :node,
-#       host: "localhost",
-#       port: 1113,
-#       username: "admin",
-#       password: "changeit",
-#       reconnect_delay: 2_000,
-#       max_attempts: :infinity
-#     ]
-#   ]
