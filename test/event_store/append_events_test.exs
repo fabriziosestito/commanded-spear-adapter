3cc214da98d2649f0aee6bec1518010dd0a7e26e
dvic <info@dvic.io>
2023-03-15 12:17:05 +0100

    Use container per test

--- test/event_store/append_events_test.exs
+++ test/event_store/append_events_test.exs
@@ -1,6 +1,6 @@
 defmodule Commanded.EventStore.Adapters.Spear.AppendEventsTest do
   alias Commanded.EventStore.Adapters.Spear
 
-  use Commanded.SpearTestCase
+  use Commanded.SpearTestCase, async: true
   use Commanded.EventStore.AppendEventsTestCase, event_store: Spear
 end
--- test/event_store/event_store_prefix_test.exs
+++ test/event_store/event_store_prefix_test.exs
@@ -2,14 +2,24 @@ defmodule Commanded.EventStore.Adapters.Spear.EventStorePrefixTest do
   alias Commanded.EventStore.Adapters.Spear
   alias Commanded.SpearTestCase
 
+  use ExUnit.Case
+
+  setup_all do
+    %{event_store_db_uri: event_store_db_uri} = TestUtils.EventStoreDBContainer.start()
+    Process.put({__MODULE__, :event_store_db_uri}, event_store_db_uri)
+    :ok
+  end
+
   use Commanded.EventStore.EventStorePrefixTestCase, event_store: Spear
 
   def start_event_store(config) do
-    config =
-      Keyword.update!(config, :prefix, fn prefix ->
-        "commandedtest" <> prefix <> Test.UUID.uuid4()
-      end)
+    name = Keyword.fetch!(config, :name)
+    stream_prefix = Keyword.fetch!(config, :prefix)
+    event_store_db_uri = Process.get({__MODULE__, :event_store_db_uri})
 
-    SpearTestCase.start_event_store(config)
+    %{event_store_meta: event_store_meta} =
+      SpearTestCase.start_event_store(name, stream_prefix, event_store_db_uri)
+
+    {:ok, event_store_meta}
   end
 end
