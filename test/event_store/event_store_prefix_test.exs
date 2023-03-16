f20a3fc2e5618b5a3ec404124ac21231ca740521
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-16 13:29:34 +0100

    Simplify test container setup (#50)

--- test/event_store/event_store_prefix_test.exs
+++ test/event_store/event_store_prefix_test.exs
@@ -4,25 +4,13 @@ defmodule Commanded.EventStore.Adapters.Spear.EventStorePrefixTest do
 
   use ExUnit.Case
 
-  setup_all do
-    %{event_store_db_uri: event_store_db_uri} = TestUtils.EventStoreDBContainer.start()
-
-    Application.put_env(:commanded_spear_adapter, __MODULE__,
-      event_store_db_uri: event_store_db_uri
-    )
-
-    :ok
-  end
-
   use Commanded.EventStore.EventStorePrefixTestCase, event_store: Spear
 
   def start_event_store(config) do
+    %{event_store_db_uri: event_store_db_uri} = TestUtils.EventStoreDBContainer.start()
+
     name = Keyword.fetch!(config, :name)
     stream_prefix = Keyword.fetch!(config, :prefix)
-
-    event_store_db_uri =
-      Application.fetch_env!(:commanded_spear_adapter, __MODULE__)
-      |> Keyword.fetch!(:event_store_db_uri)
 
     %{event_store_meta: event_store_meta} =
       SpearTestCase.start_event_store(name, stream_prefix, event_store_db_uri)
