d1561aaa2efb008e4a2569baf4f16d6e86d9ce73
dvic <info@dvic.io>
2023-03-16 20:24:54 +0100

    Filter based on stream_id instead of event type

--- lib/commanded/event_store/adapters/spear/event_publisher.ex
+++ lib/commanded/event_store/adapters/spear/event_publisher.ex
@@ -82,25 +82,45 @@ defmodule Commanded.EventStore.Adapters.Spear.EventPublisher do
     {:noreply, state, {:continue, :subscribe}}
   end
 
-  # This is a workaround to skip system events.
-  # Using a filter would be better, but for some reason the filter causes no events to be received.
-  defp process_push(%Spear.Event{type: "$>"}, _) do
-    :ok
-  end
-
-  # Same workaround as above, but for links.
-  defp process_push(%Spear.Event{link: %Spear.Event{type: "$>"}}, _) do
-    :ok
-  end
-
-  defp process_push(push, %State{
+  defp process_push(%Spear.Event{} = event, %State{
          serializer: serializer,
          stream_prefix: stream_prefix,
-         pubsub: pubsub
+         pubsub: pubsub,
+         stream_name: listening_stream
        }) do
-    push
-    |> Mapper.to_recorded_event(serializer, stream_prefix)
-    |> publish(pubsub)
+    # This is a workaround to skip system events.
+    # Using a filter would be better, but for some reason the filter causes no events to be received.
+
+    action =
+      case event do
+        %Spear.Event{metadata: %{stream_name: "$" <> _}} ->
+          # if the event itself is on a system stream, ignore it
+          :ignore
+
+        %Spear.Event{link: %Spear.Event{metadata: %{stream_name: "$" <> _ = stream_name}}} ->
+          # if the event is a link on system stream, only process it when it's the stream we are listing on
+          # this is the case for the 'all' stream when using a prefix
+          if stream_name == listening_stream do
+            :process
+          else
+            :ignore
+          end
+
+        %Spear.Event{} ->
+          # in all other cases, process the event
+          :process
+      end
+
+    case action do
+      :ignore ->
+        Logger.debug("Skipping event #{inspect(event)}")
+        :ok
+
+      :process ->
+        event
+        |> Mapper.to_recorded_event(serializer, stream_prefix)
+        |> publish(pubsub)
+    end
   end
 
   defp publish(%RecordedEvent{} = recorded_event, pubsub) do
