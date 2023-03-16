d74425e0227b90a77c0282b38ad011b1bd4c7e1a
dvic <info@dvic.io>
2023-03-16 11:43:06 +0100

    Fix test

--- test/event_store/event_store_prefix_test.exs
+++ test/event_store/event_store_prefix_test.exs
@@ -6,7 +6,11 @@ defmodule Commanded.EventStore.Adapters.Spear.EventStorePrefixTest do
 
   setup_all do
     %{event_store_db_uri: event_store_db_uri} = TestUtils.EventStoreDBContainer.start()
-    Process.put({__MODULE__, :event_store_db_uri}, event_store_db_uri)
+
+    Application.put_env(:commanded_spear_adapter, __MODULE__,
+      event_store_db_uri: event_store_db_uri
+    )
+
     :ok
   end
 
@@ -15,7 +19,10 @@ defmodule Commanded.EventStore.Adapters.Spear.EventStorePrefixTest do
   def start_event_store(config) do
     name = Keyword.fetch!(config, :name)
     stream_prefix = Keyword.fetch!(config, :prefix)
-    event_store_db_uri = Process.get({__MODULE__, :event_store_db_uri})
+
+    event_store_db_uri =
+      Application.fetch_env!(:commanded_spear_adapter, __MODULE__)
+      |> Keyword.fetch!(:event_store_db_uri)
 
     %{event_store_meta: event_store_meta} =
       SpearTestCase.start_event_store(name, stream_prefix, event_store_db_uri)
